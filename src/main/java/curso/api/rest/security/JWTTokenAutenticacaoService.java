8dd6a941268353df14a340471558a64583cfdcfa
maximilianocaetano <maximilianocaetano@gmail.com>
2021-04-10 17:43:25 -0300

    JSON web token(JWT) implanta JwttokenAuthetication

---
+++ src/main/java/curso/api/rest/security/JWTTokenAutenticacaoService.java
@@ -0,0 +1,63 @@
+package curso.api.rest.security;
+
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import curso.api.rest.ApplicationContextLoad;
+import curso.api.rest.model.Usuario;
+import curso.api.rest.repository.UsuarioRepository;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Service
+@Component
+public class JWTTokenAutenticacaoService {
+
+	private static final long EXPIRATION_TIME =  172800000;
+	
+	private static final String SECRET ="*SenhaExtremamenteSecreta*";
+	
+	private static final String TOKEN_PREFIX =  "Bearer";
+	
+	private static final String HEADER_STRING = "Authorization";
+	
+	public void addAuthetication(HttpServletResponse response, String username)throws Exception {
+		
+		String JWT = Jwts.builder().setSubject(username)
+				                   .setExpiration(new Date(System.currentTimeMillis()+ 
+				                    EXPIRATION_TIME)).signWith(SignatureAlgorithm.HS512,SECRET)
+				                   .compact();
+		
+		String token = TOKEN_PREFIX + " " + JWT;
+		response.addHeader(HEADER_STRING, token);
+		response.getWriter().write("{\"Authorization\": \""+token+"\"}");		
+	}
+	
+	public UsernamePasswordAuthenticationToken getAuthenticateAction(HttpServletRequest request) {
+		
+		String token = request.getHeader(HEADER_STRING);
+		
+		if(token != null) {
+			String user = Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token.replace(TOKEN_PREFIX, "")).getBody()
+					.getSubject();
+			if(user !=null) {			
+			Usuario usuario = ApplicationContextLoad.getApplicationContext().getBean(UsuarioRepository.class)
+					          .findByLogin(user);
+			if(usuario != null) {
+				return new UsernamePasswordAuthenticationToken(
+						usuario.getLogin(),
+						usuario.getSenha(),
+						usuario.getAuthorities());
+			}
+		}
+	}
+	return null;
+	
+  }
+}
---
+++ src/main/java/curso/api/rest/service/ImplematacaoUserDetailsService.java
@@ -0,0 +1,31 @@
+package curso.api.rest.service;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import curso.api.rest.model.Usuario;
+import curso.api.rest.repository.UsuarioRepository;
+
+@Service
+public class ImplematacaoUserDetailsService  implements UserDetailsService{
+
+	private UsuarioRepository usuarioRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		Usuario usuario =  usuarioRepository.findByLogin(username);
+		
+		if(usuario == null) {
+			throw new UsernameNotFoundException("Usuarios não encotrado!!");
+		}
+
+		return new User(usuario.getLogin(),
+				        usuario.getPassword(),
+				        usuario.getAuthorities());
+	}
+
+}
