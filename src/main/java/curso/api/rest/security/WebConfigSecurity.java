4796fea9dbab13302efffa388c6c98888526b52b
maximilianocaetano <maximilianocaetano@gmail.com>
2021-04-11 01:14:58 -0300

    JWT Classe JwtApiAutenticacaoFilter

--- src/main/java/curso/api/rest/security/WebConfigSecurity.java
+++ src/main/java/curso/api/rest/security/WebConfigSecurity.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -23,10 +24,16 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		
 		http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+	
 		.disable().authorizeRequests().antMatchers("/").permitAll()
-		.antMatchers("/index").permitAll().anyRequest().authenticated().and().logout().logoutSuccessUrl("/index")
-		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));		
 		
+		.antMatchers("/index").permitAll().anyRequest().authenticated().and().logout().logoutSuccessUrl("/index")
+		
+		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+		
+		.and().addFilterBefore(new JTWLoginFilter("/login", authenticationManager()),UsernamePasswordAuthenticationFilter.class)
+		
+		.addFilterBefore(new JwtApiAutenticacaoFilter(),UsernamePasswordAuthenticationFilter.class);
 	}
 	
 	@Override
